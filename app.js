b5832c2d91e06bca6bb22cad1ed5d2720bfb8f94
志垣大智 <shigakidaichi@shigakitaichinoMacBook.local>
2018-07-15 11:35:31 +0900

    da

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
+// modules
 var http = require('http')
 var express = require('express')
-var app = express()
 var path = require('path')
+
+var app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get("/", function(req, res, next){
   return res.render('index', {title:"dadada"});
@@ -11,9 +16,6 @@ app.get("/", function(req, res, next){
 app.get("/express", function(req, res, next){
   return res.send("expressjs");
 });
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 var server = http.createServer(app);
 server.listen('3000');
